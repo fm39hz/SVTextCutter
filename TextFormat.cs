e79080fc753eaadeb5b713398f20f39d324da356
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-02-01 22:36:09 +0700

    feat: Created text format class

---
+++ TextFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace SVTextCutter.Format;
+public abstract class Header
+{
+	public string? Target { get; set; }
+	public int FormatVersion { get; set; }
+	public bool Hidef { get; set; }
+	public bool Compressed { get; set; }
+}
+
+public abstract class Reader
+{
+	public string? Type { get; set; }
+	public int Version { get; set; }
+}
+
+public class DefaultTextFormat
+{
+	public Header? Header { get; set; }
+	public List<Reader>? Readers { get; set; }
+	public Dictionary<string, string>? Content { get; set; }
+}
