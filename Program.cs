128a0c84be5caecf234af2661eb5c153380bae23
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-02-01 22:37:14 +0700

    feat: Implemented fake running process

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿namespace SVTextCutter;
+﻿using SVTextCutter.Worker;
+
+namespace SVTextCutter;
 
 public static class Program
 {
 	public static void Main()
 	{
 		Console.WriteLine("Stardew Valley text cutter tool");
+		JsonTextHandler.StringCutting("");
 	}
 }
