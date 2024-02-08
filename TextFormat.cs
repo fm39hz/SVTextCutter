53aaafd0b25c2f1f57b2274c8f24fb5681f2700b
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-02-08 20:31:46 +0700

    refactor: clean up unnecessary symbol

--- TextFormat.cs
+++ TextFormat.cs
@@ -18,5 +18,5 @@ public sealed class DefaultTextFormat
 {
 	public Header? Header { get; set; }
 	public List<Reader>? Readers { get; set; }
-	public Dictionary<string, string>? Content { get; } = new();
+	public Dictionary<string, string> Content { get; } = new();
 }
