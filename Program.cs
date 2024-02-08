686895a62f1eafcd0000ec7809400d825714e23d
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-02-08 20:32:28 +0700

    feat: reimplement new method

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ public static class Program
 	public static void Main()
 	{
 		Console.WriteLine("Stardew Valley text cutter tool");
-		foreach (var _item in JsonTextHandler.GetAllStrings(Directory.GetCurrentDirectory() + "/Project"))
+		foreach (var _item in TextHandler.GetAllText(Directory.GetCurrentDirectory() + "/Project"))
 		{
-			JsonTextHandler.SplitTextValue(_item);
+			var _content = TextHandler.GetContentValue(_item);
+			Console.WriteLine(_content);
 		}
 	}
 }
