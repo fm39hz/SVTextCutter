7d19bb76b58b47b04cdc78a10f7b41aaa5fa0b06
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-02-11 22:54:05 +0700

    feat: implement new TextHandler

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ public static class Program
 	public static void Main()
 	{
 		Console.WriteLine("Stardew Valley text cutter tool");
-		TextHandler.GetAllText(Directory.GetCurrentDirectory() + "/Project");
+		TextHandler.GetAllText();
+		TextHandler.ExportFile();
 	}
 }
